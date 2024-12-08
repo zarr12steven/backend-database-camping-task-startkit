22dc0335b3503f8f25acbd8ca6da299db98cb01d
Daniel Lin <zarr12steven@hotmail.com>
2024-12-08 16:32:07 +0800

    update task 1-4

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -67,6 +67,10 @@ WHERE email = 'opacity0@hexschooltest.io';
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 
+-- 查詢
+SELECT COUNT(*) AS USERS_COUNT
+FROM "USER";
+
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 
 
