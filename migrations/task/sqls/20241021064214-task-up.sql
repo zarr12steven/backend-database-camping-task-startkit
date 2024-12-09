80273a9aead2c93d7462994be89068432d8d28ea
Daniel Lin <zarr12steven@hotmail.com>
2024-12-09 11:32:53 +0800

    update task 5-3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -345,6 +345,16 @@ AND
     -- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
     -- 3. 狀態`status` 設定為即將授課
 
+-- 新增
+INSERT INTO "COURSE_BOOKING" (user_id, course_id, booking_at, status)
+VALUES
+  (
+    (SELECT id FROM "USER" WHERE name='王小明'),
+    (SELECT id FROM "COURSE" WHERE user_id = (SELECT id FROM "USER" WHERE name='李燕容')),
+    '2024-11-24 17:10:25',
+    '即將授課'
+  );
+
 -- 5-4. 查詢：取得王小明所有的預約紀錄，包含取消預約的紀錄
 
 -- 5-5. 修改：`王小明` 現在已經加入直播室了，請在`COURSE_BOOKING`更新該筆預約資料（請注意，不要更新到已經取消的紀錄）：
